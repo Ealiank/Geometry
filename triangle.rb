82d8ce4f9026107ef5c37df08d6f7194330a4d29
Ealian Kowalsky <ealiank@icloud.com>
2021-06-01 11:31:11 -0400

    Adding functions to calculate c, height, perimeter and area.

--- triangle.rb
+++ triangle.rb
@@ -5,4 +5,20 @@ class Triangle
     @a = a
     @b = b
   end
+
+  def c
+    (a * a) + (b * b)
+  end
+
+  def height
+    (a * b) * 2
+  end
+
+  def perimeter
+    a + b + c
+  end
+
+  def area
+    (h * b) * 2
+  end
 end
